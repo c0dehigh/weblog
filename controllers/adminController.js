431f0cd83ec5cb67c26ecd6ec83c51b6329c4090
hamed zibandeh <high_dev@outlook.com>
2023-02-03 21:28:29 +0330

    manage errors

---
+++ controllers/adminController.js
@@ -0,0 +1,35 @@
+const Blog = require("../models/Blog");
+const { get500 } = require("./errorController");
+const { get } = require("mongoose");
+
+exports.getDashboard = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ user: req.user.id });
+    res.render("private/blogs", {
+      pageTitle: "Admin dashboard",
+      path: "/dashboard",
+      layout: "./layouts/dashLayout",
+      fullname: req.user.fullname,
+      blogs,
+    });
+  } catch (err) {
+    get500(req, res);
+  }
+  exports.getAddPost = (req, res) => {
+    res.render("private/addPost", {
+      pageTitle: "Create new post",
+      path: "/dashboard/add-post",
+      layout: "./layouts/dashLayout",
+      fullname: req.user.fullname,
+    });
+  };
+
+  exports.createPost = async (req, res) => {
+    try {
+      await Blog.create({ ...req.body, user: req.user.id });
+      res.redirect("/dashboard");
+    } catch (err) {
+      get500(req, res);
+    }
+  };
+};
