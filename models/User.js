351d43b2a8edff21d460f13736afc69add9c661c
hamed zibandeh <high_dev@outlook.com>
2023-02-03 18:13:19 +0330

    change method Schema users

--- models/User.js
+++ models/User.js
@@ -44,6 +44,4 @@ userSchema.pre("save", function (next) {
   });
 });
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
