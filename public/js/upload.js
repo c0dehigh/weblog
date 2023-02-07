097bcbda8328f01565452aa46cbfc19e71f8e1de
hamed zibandeh <high_dev@outlook.com>
2023-02-07 11:09:06 +0330

    add event progressbar

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,16 +1,35 @@
 document.getElementById("imageUpload").onclick = function () {
   let xhttp = new XMLHttpRequest();
 
+  const selectedImage = document.getElementById("selectedImage");
+  const imageStatus = document.getElementById("imageStatus");
+  const progressDiv = document.getElementById("progressDiv");
+  const progressBar = document.getElementById("progressBar");
+
   xhttp.onreadystatechange = function () {
-    if (this.status === 200) {
-      document.getElementById("imageStatus").innerHTML = this.responseText;
-    } else {
-      document.getElementById("imageStatus").innerHTML = "Problem from server";
-    }
+    imageStatus.innerHTML = this.responseText;
   };
   xhttp.open("post", "/dashboard/image-upload");
+
+  xhttp.upload.onprogress = function (e) {
+    if (e.lengthComputable) {
+      let result = Math.floor((e.loaded / e.total) * 100);
+      if (result !== 100) {
+        progressBar.innerHTML = result + "%";
+        progressBar.style = "width:" + result + "%";
+      } else {
+        progressDiv.style = "display: none";
+      }
+    }
+  };
+
   let formData = new FormData();
 
-  formData.append("image", document.getElementById("selectedImage").files[0]);
-  xhttp.send(formData);
+  if (selectedImage.files.length > 0) {
+    progressDiv.style = "display : block";
+    formData.append("image", selectedImage.files[0]);
+    xhttp.send(formData);
+  } else {
+    imageStatus.innerHTML = "Select image for upload !";
+  }
 };
