2b84e86c5ea54589f9999400872700ce5fc2f94b
hamed zibandeh <high_dev@outlook.com>
2023-02-08 22:32:30 +0330

    select element for result

--- public/js/upload.js
+++ public/js/upload.js
@@ -5,9 +5,16 @@ document.getElementById("imageUpload").onclick = function () {
   const imageStatus = document.getElementById("imageStatus");
   const progressDiv = document.getElementById("progressDiv");
   const progressBar = document.getElementById("progressBar");
+  const uplaodResult = document.getElementById("uploadResult")
 
   xhttp.onreadystatechange = function () {
-    imageStatus.innerHTML = this.responseText;
+    if(xhttp.status === 200){
+      imageStatus.innerHTML = "Image uploaded successful"
+      uplaodResult.innerHTML = this.responseText
+    } else {
+      imageStatus.innerHTML = this.responseText
+    }
+
   };
   xhttp.open("post", "/dashboard/image-upload");
 
